35115f794d0238a5eeed9eac3107efc71874db36
navruzoffd <navruzovdaniyal04@gmail.com>
2024-10-18 10:05:48 +0300

    update

---
+++ nvim/lua/user/lsp.lua
@@ -0,0 +1,20 @@
+vim.diagnostic.config({
+    virtual_text = false,  -- Отключаем отображение ошибок справа от строки
+    signs = true,          -- Оставляем иконки ошибок в левой колонке
+    update_in_insert = false,
+    underline = true,
+    severity_sort = true,
+    float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+    },
+})
+
+vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
